d6348fa2fee710b99ee5da980eba6ab5dc494488
Diego B <diegoalexander_15@hotmail.com>
2022-02-12 10:59:15 -0500

    ajustes a la ruta para produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true
 };
-export const UrlWs = "http://localhost:9100/api";
-export const UrlWsFiles = "http://localhost:9100/files";
+export const UrlWs = "http://186.4.146.196:9101/api";
+export const UrlWsFiles = "http://186.4.146.196:9101/files";
